f06f142114e1d64f007217c55cac28cdf2c40170
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2026-02-07 21:16:53 +0530

    Delete components directory

--- components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Button({ children, onClick, className = '' }: { children: React.ReactNode; onClick?: () => void; className?: string }) {
-  return (
-    <button className={`btn ${className}`} onClick={onClick}>{children}</button>
-  )
-}
